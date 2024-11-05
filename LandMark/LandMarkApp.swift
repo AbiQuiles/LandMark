c5b979f45d07d116bc4ffc57b00b6e3b55f2e425
Abiezer Quiles <abiquiles69@gmail.com>
2024-11-05 15:19:57 -0500

    Initial Commit

---
+++ LandMark/LandMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarkApp.swift
+//  LandMark
+//
+//  Created by Abiezer Quiles on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct LandMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LandMarkTests/LandMarkTests.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarkTests.swift
+//  LandMarkTests
+//
+//  Created by Abiezer Quiles on 11/5/24.
+//
+
+import Testing
+@testable import LandMark
+
+struct LandMarkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
